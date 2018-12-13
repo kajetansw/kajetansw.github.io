c69851226035c389d2563e87634644eae565776b
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2018-12-13 21:04:23 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+   items: 1,
+   loop: true,
+   margin: 10,
+   autoplay: true,
+   autoplayTimeout: 3000,
+   autoplayHoverPause: true,
+   // check if transition time is configurable
+   center: true
+});
+$('.play').on('click', function () {
+   owl.trigger('play.owl.autoplay', [1000])
+});
+$('.stop').on('click', function () {
+   owl.trigger('stop.owl.autoplay')
+});
