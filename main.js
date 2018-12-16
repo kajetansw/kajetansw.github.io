2b71baa939826d22e62af09f5b471d5485858c92
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2018-12-16 11:47:20 +0100

    created skills card with dummy content,

--- main.js
+++ main.js
@@ -129,3 +129,44 @@ particlesJS("particles", {
    },
    "retina_detect": true
 });
+
+
+
+
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+const progressbars = document.getElementsByClassName('progressbar');
+for (const progressbar of progressbars) {
+   const bar = new ProgressBar.Circle(progressbar, {
+      color: '#aaa',
+      // This has to be the same size as the maximum width to
+      // prevent clipping
+      strokeWidth: 4,
+      trailWidth: 1,
+      easing: 'easeInOut',
+      duration: 1400,
+      text: {
+         autoStyleContainer: false
+      },
+      from: { color: '#aaa', width: 1 },
+      to: { color: '#ddd', width: 4 },
+      // Set default step function for all animate calls
+      step: function (state, circle) {
+         circle.path.setAttribute('stroke', state.color);
+         circle.path.setAttribute('stroke-width', state.width);
+
+         let value = Math.round(circle.value() * 100);
+         if (value === 0) {
+            circle.setText('');
+         } else {
+            circle.setText(`${value}%`);
+         }
+
+      }
+   });
+   bar.text.style.fontFamily = '"Quicksand", sans-serif';
+   bar.text.style.fontSize = '1.8rem';
+
+   bar.animate(.8);  // Number from 0.0 to 1.0
+}
