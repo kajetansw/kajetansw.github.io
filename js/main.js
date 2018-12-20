ba3870ee421f2efb0c18ce91365717fc743537a5
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2018-12-20 21:10:08 +0100

    added prototype of experience and education timelines

--- js/main.js
+++ js/main.js
@@ -1,135 +1,131 @@
 // Owl-Carousel init
-var owl = $('.owl-carousel');
+var owl = $(".owl-carousel");
 owl.owlCarousel({
-   items: 1,
-   loop: true,
-   margin: 10,
-   autoplay: true,
-   autoplayTimeout: 2500,
-   smartSpeed: 800,
-   // autoplayHoverPause: true,
-   center: true
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    smartSpeed: 800,
+    // autoplayHoverPause: true,
+    center: true
 });
-$('.play').on('click', function () {
-   owl.trigger('play.owl.autoplay', [1000])
+$(".play").on("click", function() {
+    owl.trigger("play.owl.autoplay", [1000]);
 });
-$('.stop').on('click', function () {
-   owl.trigger('stop.owl.autoplay')
+$(".stop").on("click", function() {
+    owl.trigger("stop.owl.autoplay");
 });
 
 // ParticlesJS init
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS("particles", {
-   "particles": {
-      "number": {
-         "value": 80,
-         "density": {
-            "enable": true,
-            "value_area": 800
-         }
-      },
-      "color": {
-         "value": "#ffffff"
-      },
-      "shape": {
-         "type": "circle",
-         "stroke": {
-            "width": 0,
-            "color": "#000000"
-         },
-         "polygon": {
-            "nb_sides": 5
-         },
-         "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
-         }
-      },
-      "opacity": {
-         "value": 0.5,
-         "random": false,
-         "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-         }
-      },
-      "size": {
-         "value": 3,
-         "random": true,
-         "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
-         }
-      },
-      "line_linked": {
-         "enable": true,
-         "distance": 150,
-         "color": "#ffffff",
-         "opacity": 0.4,
-         "width": 1
-      },
-      "move": {
-         "enable": true,
-         "speed": 6,
-         "direction": "none",
-         "random": false,
-         "straight": false,
-         "out_mode": "out",
-         "bounce": false,
-         "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-         }
-      }
-   },
-   "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-         "onhover": {
-            "enable": false,
-            "mode": "repulse"
-         },
-         "onclick": {
-            "enable": false,
-            "mode": "push"
-         },
-         "resize": true
-      },
-      "modes": {
-         "grab": {
-            "distance": 400,
-            "line_linked": {
-               "opacity": 1
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
             }
-         },
-         "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-         },
-         "repulse": {
-            "distance": 200,
-            "duration": 0.4
-         },
-         "push": {
-            "particles_nb": 4
-         },
-         "remove": {
-            "particles_nb": 2
-         }
-      }
-   },
-   "retina_detect": true
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
+            },
+            polygon: {
+                nb_sides: 5
+            },
+            image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: false,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: false,
+                mode: "push"
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: true
 });
-
-
-
-
--- js/progressbars.js
+++ js/progressbars.js
@@ -1,24 +1,23 @@
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-
 /*
 ##########################################
    SETTINGS
 ##########################################
 */
 const fontFamily = '"Quicksand", sans-serif';
-const fontSize = '1.8rem';
+const fontSize = "1.8rem";
 const stepHandler = (state, circle) => {
-   circle.path.setAttribute('stroke', state.color);
-   circle.path.setAttribute('stroke-width', state.width);
+    circle.path.setAttribute("stroke", state.color);
+    circle.path.setAttribute("stroke-width", state.width);
 
-   let value = Math.round(circle.value() * 100);
-   if (value === 0) {
-      circle.setText('');
-   } else {
-      circle.setText(`${value}%`);
-   }
+    let value = Math.round(circle.value() * 100);
+    if (value === 0) {
+        circle.setText("");
+    } else {
+        circle.setText(`${value}%`);
+    }
 };
 
 /*
@@ -26,137 +25,137 @@ const stepHandler = (state, circle) => {
    PROGRESSBAR FOR ANGULAR SKILL
 ##########################################
 */
-const progressbarAngular = document.querySelector('.progressbar-angular');
+const progressbarAngular = document.querySelector(".progressbar-angular");
 const barAngular = new ProgressBar.Circle(progressbarAngular, {
-   color: '#aaa',
-   // This has to be the same size as the maximum width to
-   // prevent clipping
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#b52e31', width: 5 },
-   // Set default step function for all animate calls
-   step: stepHandler
+    color: "#aaa",
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#b52e31", width: 5 },
+    // Set default step function for all animate calls
+    step: stepHandler
 });
 barAngular.text.style.fontFamily = fontFamily;
 barAngular.text.style.fontSize = fontSize;
-barAngular.animate(.9);  // Number from 0.0 to 1.0
+barAngular.animate(0.9); // Number from 0.0 to 1.0
 
 /*
 ##########################################
    PROGRESSBAR FOR TS/JS SKILL
 ##########################################
 */
-const progressbarTsJs = document.querySelector('.progressbar-tsjs');
+const progressbarTsJs = document.querySelector(".progressbar-tsjs");
 const barTsJs = new ProgressBar.Circle(progressbarTsJs, {
-   color: '#aaa',
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#007acc', width: 5 },
-   step: stepHandler
+    color: "#aaa",
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#007acc", width: 5 },
+    step: stepHandler
 });
 barTsJs.text.style.fontFamily = fontFamily;
 barTsJs.text.style.fontSize = fontSize;
-barTsJs.animate(.85);
+barTsJs.animate(0.85);
 
 /*
 ##########################################
    PROGRESSBAR FOR SPRING/HIBERNATE SKILL
 ##########################################
 */
-const progressbarSpring = document.querySelector('.progressbar-spring');
+const progressbarSpring = document.querySelector(".progressbar-spring");
 const barSpring = new ProgressBar.Circle(progressbarSpring, {
-   color: '#aaa',
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#77BC1F', width: 5 },
-   step: stepHandler
+    color: "#aaa",
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#77BC1F", width: 5 },
+    step: stepHandler
 });
 barSpring.text.style.fontFamily = fontFamily;
 barSpring.text.style.fontSize = fontSize;
-barSpring.animate(.8);
+barSpring.animate(0.8);
 
 /*
 ##########################################
    PROGRESSBAR FOR DATABASES SKILL
 ##########################################
 */
-const progressbarDb = document.querySelector('.progressbar-db');
+const progressbarDb = document.querySelector(".progressbar-db");
 const barDb = new ProgressBar.Circle(progressbarDb, {
-   color: '#aaa',
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#E58F00', width: 5 },
-   step: stepHandler
+    color: "#aaa",
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#E58F00", width: 5 },
+    step: stepHandler
 });
 barDb.text.style.fontFamily = fontFamily;
 barDb.text.style.fontSize = fontSize;
-barDb.animate(.7);
+barDb.animate(0.7);
 
 /*
 ##########################################
    PROGRESSBAR FOR CLEAN CODE SKILL
 ##########################################
 */
-const progressbarCleanCode = document.querySelector('.progressbar-cleancode');
+const progressbarCleanCode = document.querySelector(".progressbar-cleancode");
 const barCleanCode = new ProgressBar.Circle(progressbarCleanCode, {
-   color: '#aaa',
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#862d8e', width: 5 },
-   step: stepHandler
+    color: "#aaa",
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#862d8e", width: 5 },
+    step: stepHandler
 });
 barCleanCode.text.style.fontFamily = fontFamily;
 barCleanCode.text.style.fontSize = fontSize;
-barCleanCode.animate(.9);
+barCleanCode.animate(0.9);
 
 /*
 ##########################################
    PROGRESSBAR FOR PASSION SKILL
 ##########################################
 */
-const progressbarPassion = document.querySelector('.progressbar-passion');
+const progressbarPassion = document.querySelector(".progressbar-passion");
 const barPassion = new ProgressBar.Circle(progressbarPassion, {
-   color: '#aaa',
-   strokeWidth: 4,
-   trailWidth: 1,
-   easing: 'easeInOut',
-   duration: 1400,
-   text: {
-      autoStyleContainer: false
-   },
-   from: { color: '#aaa', width: 1 },
-   to: { color: '#fff', width: 5 },
-   step: stepHandler
+    color: "#aaa",
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: "#aaa", width: 1 },
+    to: { color: "#fff", width: 5 },
+    step: stepHandler
 });
 barPassion.text.style.fontFamily = fontFamily;
 barPassion.text.style.fontSize = fontSize;
